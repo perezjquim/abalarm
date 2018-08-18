7502987c46401695279ed661d7b9d937d1208be2
Joaquim Perez <jquim1@hotmail.com>
2018-08-18 01:15:24 +0100

    app init / added alarms for alerts / dialog layout

---
+++ app/src/main/java/com/perezjquim/abalarm/DoseAlert.java
@@ -0,0 +1,18 @@
+package com.perezjquim.abalarm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.perezjquim.UIHelper;
+
+public class DoseAlert extends BroadcastReceiver
+{
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        System.out.println("! Dose !");
+
+        UIHelper.notify(context, "AB Alarm", "Time to take your anti-biotic!");
+    }
+}
--- app/src/main/java/com/perezjquim/abalarm/MainActivity.java
+++ app/src/main/java/com/perezjquim/abalarm/MainActivity.java
@@ -1,7 +1,20 @@
 package com.perezjquim.abalarm;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TimePicker;
+
+import com.perezjquim.AlarmHelper;
+import com.perezjquim.SharedPreferencesHelper;
+import com.perezjquim.UIHelper;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import static com.perezjquim.UIHelper.toast;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -10,5 +23,52 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void setAlarm(View v)
+    {
+        SeekBar seekBar = findViewById(R.id.interval);
+        int interval = seekBar.getProgress() + 1;
+        //System.out.println("Interval: " + interval + " hours");
+
+        SharedPreferencesHelper prefs = new SharedPreferencesHelper(this);
+        prefs.setInt("config","interval", interval);
+
+        Calendar now = Calendar.getInstance();
+        //System.out.println("Now: "+now.getTime().toString());
+
+        TimePicker picker = findViewById(R.id.begintime);
+
+        Calendar firstDose = (Calendar) now.clone();
+        firstDose.set(Calendar.HOUR_OF_DAY, picker.getCurrentHour());
+        firstDose.set(Calendar.MINUTE, picker.getCurrentMinute());
+        firstDose.set(Calendar.SECOND, 0);
+        //System.out.println("First dose: "+firstDose.getTime().toString());
+
+        long remainingTime;
+
+        if(now.before(firstDose))
+        {
+            remainingTime = firstDose.getTimeInMillis() - now.getTimeInMillis();
+
+            toast(this,"Alarm scheduled!");
+
+            toast(this,"First dose in " + remainingTime / (1000 * 60) + " minutes");
+            //System.out.println("First dose in " + remainingTime / (1000 * 60) + " minutes");
+        }
+        else
+        {
+            Calendar nextDose = (Calendar) firstDose.clone();
+            nextDose.add(Calendar.HOUR_OF_DAY, interval);
+
+            remainingTime = nextDose.getTimeInMillis() - now.getTimeInMillis();
+
+            toast(this,"Alarm scheduled!");
+
+            toast(this,"Next dose in " + remainingTime / (1000 * 60) + " minutes");
+            //System.out.println("Next dose in " + remainingTime / (1000 * 60) + " minutes");
+        }
+
+        AlarmHelper.scheduleStandardAlarm(this,"FirstDoseAlert", remainingTime);
     }
 }
